e3e2d721a4f9d125f257462c229524143852fe4b
Marcio Leite <marcioleite09@gmail.com>
2020-07-05 08:59:18 -0300

    Consulta com vários critérios

--- src/main/java/com/msleite0508/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/msleite0508/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.msleite0508.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,12 +9,14 @@ import org.springframework.stereotype.Repository;
 
 import com.msleite0508.workshopmongo.domain.Post;
 
-
-
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-	List<Post> searchTitle(String text);	
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
